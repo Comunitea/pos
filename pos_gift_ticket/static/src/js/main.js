8a54f0a0a3dffb2899297bbcbfb0661c1c313525
Javier <javierjcf@gmail.com>
2018-12-24 13:34:02 +0100

    [FIX]pos_gift_ticket: Imprimir ticket regalo por impresora proxy

--- pos_gift_ticket/static/src/js/main.js
+++ pos_gift_ticket/static/src/js/main.js
@@ -26,8 +26,35 @@
         print_gift: function() {
             // Render the PosTicketGift in order to print it
             this.$('.pos-receipt-container').html(QWeb.render('PosTicketGift', this.get_receipt_render_env()));
-            this.print_web()
-            this.printing_gift_ticket = false;
+            if (!this.pos.config.iface_print_via_proxy) { // browser (html) printing
+
+                // The problem is that in chrome the print() is asynchronous and doesn't
+                // execute until all rpc are finished. So it conflicts with the rpc used
+                // to send the orders to the backend, and the user is able to go to the next 
+                // screen before the printing dialog is opened. The problem is that what's 
+                // printed is whatever is in the page when the dialog is opened and not when it's called,
+                // and so you end up printing the product list instead of the receipt... 
+                //
+                // Fixing this would need a re-architecturing
+                // of the code to postpone sending of orders after printing.
+                //
+                // But since the print dialog also blocks the other asynchronous calls, the
+                // button enabling in the setTimeout() is blocked until the printing dialog is 
+                // closed. But the timeout has to be big enough or else it doesn't work
+                // 1 seconds is the same as the default timeout for sending orders and so the dialog
+                // should have appeared before the timeout... so yeah that's not ultra reliable. 
+    
+                this.lock_screen(true);
+    
+                setTimeout(function(){
+                    self.lock_screen(false);
+                }, 1000);
+    
+                this.print_web();
+            } else {    // proxy (xml) printing
+                this.print_xml();
+                this.lock_screen(false);
+            }
 
         },
         renderElement: function() {
